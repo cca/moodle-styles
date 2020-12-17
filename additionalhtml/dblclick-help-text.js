20b1f39058e957b35baeeeee78a303b0909951de
phette23 <phette23@gmail.com>
2020-12-17 12:01:31 -0800

    Moodle alert when help text is copied
    use Moodle core function M.core.alert()

--- additionalhtml/dblclick-help-text.js
+++ additionalhtml/dblclick-help-text.js
@@ -4,9 +4,14 @@ function copyHelpToClipboard(event) {
     if (content) {
         // strip HTML tags
         content = content.replace(/(<([^>]+)>)/gi, "")
-        navigator.clipboard.writeText(content).then(function() {
-            console.log('text copied to clipboard')
-        }, function () {
+        navigator.clipboard.writeText(content).then(()=>{
+            if (M && M.core && M.core.alert) {
+                new M.core.alert({
+                    'title': 'Help text copied',
+                    'message': 'Double-clicking a help icon <i class="icon fa fa-question-circle text-info fa-fw"></i>will copy its text to your clipboard.'
+                })
+            }
+        }, ()=>{
             console.error('error in copying text to clipboard')
         })
     }
