fcca5e86a0ecc729bc213ea81fa764ae71859be6
phette23 <phette23@gmail.com>
2021-01-20 09:14:50 -0800

    draft of libraryh3lp chat in Moodle

---
+++ additionalhtml/chat.js
@@ -0,0 +1,17 @@
+// use libraryh3lp Presence API https://dev.libraryh3lp.com/presence.html
+fetch('https://libraryh3lp.com/presence/jid/cca-instructional-support/chat.libraryh3lp.com/text')
+    .then(resp => resp.text())
+    .then(status => {
+        // only display if we are signed in
+        if (status == 'available' || status == 'chat') {
+            // jQuery not available yet
+            let div = document.createElement('div')
+            div.className = 'needs-js'
+            document.querySelector('body').append(div)
+            let x = document.createElement("script");
+            x.async = true;
+            x.src = "https://libraryh3lp.com/js/libraryh3lp.js?17190";
+            let y = document.getElementsByTagName("script")[0];
+            y.parentNode.insertBefore(x, y);
+        }
+    })
