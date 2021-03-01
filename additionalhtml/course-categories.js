1f6517b81da44914c3d6df09a716648a6ef33de4
Eric Phetteplace <phette23@gmail.com>
2021-03-01 11:45:28 -0800

    course links under course categories pg should actually go to courses

---
+++ additionalhtml/course-categories.js
@@ -0,0 +1,9 @@
+// run on course home pages
+// we don't have jQuery yet so use vanilla JS
+if (location.pathname.match('/course/management.php')) {
+    // make course titles link to course view instead of same page we're on
+    document.querySelectorAll('#course-category-listings #course-listing .coursename').forEach(course => {
+        let link = new URL(course.href)
+        course.href = `/course/view.php?id=${link.searchParams.get('courseid')}`
+    })
+}
