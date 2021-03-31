7135ffdb9b6f9aab8b673339acd983485822ebd9
Eric Phetteplace <phette23@gmail.com>
2021-03-31 13:21:51 -0700

    alert when user edits first or last name

---
+++ additionalhtml/name-change-warning.js
@@ -0,0 +1,12 @@
+// run when anyone edits a profile
+let page = location.pathname
+if (page.match('/user/edit.php') || page.match('/user/editadvanced.php')) {
+    let showNameChangeWarning = (ev) => {
+        const html = '<div class="col-md-9 alert alert-primary" style="margin:6px;">We recommend notifying your instructors or students after changing your name, to avoid any confusion.</div>'
+        // we should ahve jQuery by the time this happens
+        $(event.target.parentElement).after(html)
+    }
+    document.querySelectorAll('#id_firstname, #id_lastname').forEach(el => {
+        addEventListener('change', showNameChangeWarning, { once: true })
+    })
+}
