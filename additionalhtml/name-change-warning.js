3bbadff61fd046feeb3c70829aff59d2e5fc8a0b
Eric Phetteplace <phette23@gmail.com>
2021-03-31 13:25:06 -0700

    name change - add a close btn to the alert

--- additionalhtml/name-change-warning.js
+++ additionalhtml/name-change-warning.js
@@ -2,7 +2,7 @@
 let page = location.pathname
 if (page.match('/user/edit.php') || page.match('/user/editadvanced.php')) {
     let showNameChangeWarning = (ev) => {
-        const html = '<div class="col-md-9 alert alert-primary" style="margin:6px;">We recommend notifying your instructors or students after changing your name, to avoid any confusion.</div>'
+        const html = '<div class="col-md-9 alert alert-primary" style="margin:6px;"><button type="button" class="close" data-dismiss="alert">×</button> We recommend notifying your instructors or students after changing your name, to avoid any confusion.</div>'
         // we should ahve jQuery by the time this happens
         $(event.target.parentElement).after(html)
     }
