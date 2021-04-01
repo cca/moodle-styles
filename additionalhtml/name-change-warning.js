b820ba506da902d576769bffcd9d50c617c05e98
Eric Phetteplace <phette23@gmail.com>
2021-04-01 14:13:10 -0700

    typo in a comment

--- additionalhtml/name-change-warning.js
+++ additionalhtml/name-change-warning.js
@@ -3,7 +3,7 @@ let page = location.pathname
 if (page.match('/user/edit.php') || page.match('/user/editadvanced.php')) {
     let showNameChangeWarning = (ev) => {
         const html = '<div class="col-md-9 alert alert-primary" style="margin:6px;"><button type="button" class="close" data-dismiss="alert">×</button> We recommend notifying your instructors or students after changing your name, to avoid any confusion.</div>'
-        // we should ahve jQuery by the time this happens
+        // we should have jQuery by the time this happens
         $(event.target.parentElement).after(html)
     }
     document.querySelectorAll('#id_firstname, #id_lastname').forEach(el => {
