76bdd2430e8a42799f905ec29105e60894dfe897
phette23 <phette23@gmail.com>
2023-03-04 19:34:56 -0800

    rm babel build step, no longer necessary, add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
 const { src, dest, parallel, watch } = require('gulp')
-const babel = require("gulp-babel")
 const concat = require("gulp-concat")
 const eslint = require('gulp-eslint')
 const iife = require("gulp-iife")
 const insert = require("gulp-insert")
 const rename = require('gulp-rename')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('sass'))
 const uglify = require("gulp-uglify")
-
-sass.compiler = require('sass')
 
 const settings = {
 	dest: "build",
@@ -29,7 +26,6 @@ function addlhtml() {
     return src(settings.src.additionalhtml)
         .pipe(concat('footer.js'))
         .pipe(iife())
-        .pipe(babel({ presets: ['@babel/preset-env'] }))
         .pipe(uglify())
         .pipe(insert.prepend(`<script async src="https://www.googletagmanager.com/gtag/js?id=G-9KK2VCY0TM"></script><script>\n// minified ${new Date().toLocaleString()} - see https://github.com/cca/moodle-styles\n`))
         .pipe(insert.append('\n</script>'))
