ef3cdeb35f07f5d556eefd7ecf5d938b11874631
phette23 <phette23@gmail.com>
2023-06-29 13:19:02 -0700

    btn to lock ILA for ISS review ref #32
    this adds a primary button to the grading interface of the ILA assignment
    for international students in the Internships demo (right now, only that
    one assignment, goal is to make it work for this type of assignment in
    general) which locks the assignment, making the grading workflow a little
    smoother for this special assignment

---
+++ additionalhtml/iss-lock-btn.js
@@ -0,0 +1,19 @@
+// specifically for the ILA Assignment where the IC needs to lock the assignment
+// and then ISS recieves a notice from an event monitor, they unlock and grade
+const params = new URLSearchParams(location.search)
+// @TODO check in document.getElementById('id_grade') for <option>s that match assignment's special scale values
+// for now only works on 1 assignment in Internships Demo course
+if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 'grader' && params.get('id') == '236146') {
+    // lock URLs look like
+    // https://moodle.cca.edu/mod/assign/view.php?id=236146&userid=4453&action=lock&sesskey=vKW7E2y8Kn&page=0
+    // where ID appears to be the Assignment ID and user ID is the student's ID
+    // make sure we have all the parameters we need
+    if (params.get('id') && params.get('userid') && M && M.cfg && M.cfg.sesskey) {
+        const html = `<a href="/mod/assign/view.php?id=${params.get('id')}&userid=${params.get('userid')}&action=lock&sesskey=${M.cfg.sesskey}" class="btn btn-primary" title="This button disallows further student submissions and triggers an email notification to ISS. Push it AFTER you have Saved the grade.">Lock Submissions & Notify ISS</a>`
+        let div = document.createElement('div')
+        div.className = "form-group row fitem py-1"
+        div.innerHTML = html
+        const target = document.getElementById('fitem_id_currentgrade')
+        if (target) target.insertAdjacentElement('beforebegin', div)
+    }
+}
