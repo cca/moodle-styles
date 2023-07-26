4266d7cffd28e1e4cc97202e4cd4a0546c76c130
phette23 <phette23@gmail.com>
2023-07-26 16:43:11 -0700

    show ISS button on all assignments with the scale, closes #32

--- additionalhtmlhead/iss-lock-btn.js
+++ additionalhtmlhead/iss-lock-btn.js
@@ -1,11 +1,10 @@
 // specifically for the ILA Assignment where the IC needs to lock the assignment
 // and then ISS recieves a notice from an event monitor, they unlock and grade
 let params = new URLSearchParams(location.search)
-// @TODO check in document.getElementById('id_grade') for <option>s that match assignment's special scale values
-// for now only works on 1 assignment in Internships Demo course
-if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 'grader' && params.get('id') == '236146') {
+if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 'grader') {
     const id = 'cca-iss-btn'
-    let makebtn = () => {
+
+    const makebtn = () => {
         // the query string can change without the page reloading if you arrow to another student
         params = new URLSearchParams(location.search)
         // lock URLs look like
@@ -19,17 +18,28 @@ if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 
         return div
     }
 
-    // make sure we have all the parameters we need
-    if (params.get('id') && params.get('userid') && M && M.cfg && M.cfg.sesskey) {
-        // grading UI is quite dynamic, check DOM every second and add our button if it's not there
-        setInterval(() => {
-            let target = document.getElementById('fitem_id_currentgrade')
-            let ourbtn = document.getElementById(id)
-            let locked = document.querySelector('.submissionnoteditable')
-            // target exists, there's no button, & assignment isn't already locked
-            if (target && !ourbtn && !locked) {
-                target.insertAdjacentElement('beforebegin', makebtn())
-            }
-        }, 1000)
+    const monitorAndAddButton = () => {
+        let target = document.getElementById('fitem_id_currentgrade')
+        let ourbtn = document.getElementById(id)
+        let locked = document.querySelector('.submissionnoteditable')
+        // target exists, there's no button, & assignment isn't already locked
+        if (target && !ourbtn && !locked) {
+            target.insertAdjacentElement('beforebegin', makebtn())
+        }
     }
+
+    const checkForISSScale = () => {
+        // grading scale is like: No grade, ISS Review, ISS Approved, Incomplete
+        let grading_scale = Array.from(document.querySelectorAll('#id_grade option'))
+        if (grading_scale && grading_scale.some(g => g.textContent.trim().toLowerCase() === 'iss approved')) {
+            clearInterval(interval)
+            // make sure we have all the parameters we need
+            if (params.get('id') && params.get('userid') && M && M.cfg && M.cfg.sesskey) {
+                // grading UI is quite dynamic, check DOM every second and add our button if it's not there
+                setInterval(monitorAndAddButton, 1000)
+            }
+        }
+    }
+
+    const interval = setInterval(checkForISSScale, 500)
 }
