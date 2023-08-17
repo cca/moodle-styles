8b6caea1e3c348e5ab1aba69772f32631bb57b77
phette23 <phette23@gmail.com>
2023-08-17 08:30:08 -0700

    import warning, closes #37

--- additionalhtml/user-data-warn.js
+++ additionalhtml/user-data-warn.js
@@ -5,6 +5,18 @@ if (location.pathname.match('/backup/backup.php')) {
         let msg = `<div class="alert alert-warning col-md-9 mx-auto">Uncheck "Include enrolled users" if you are restoring this backup for a new semester. If you create a backup with user data, be sure its storage complies with <a href="https://portal.cca.edu/teaching/teaching-lab/teaching-resources/student-policies-faculty/ferpa-tutorial-for-faculty/" target="_blank">FERPA regulations</a>.</div>`
         let div = document.createElement('div')
         div.innerHTML = msg
-        document.getElementById('id_rootsettingscontainer').insertAdjacentElement('afterbegin', div)
+        let target = document.getElementById('id_rootsettingscontainer')
+        if (target) target.insertAdjacentElement('afterbegin', div)
     }
+}
+// #37 warn users about import mistakes
+if (location.pathname.match('/backup/import.php')) {
+    let msg = `<div class="alert alert-warning col-md-9 mx-auto">
+Before import, make sure you are in your destination course, not your origin course.<br>
+If you previously attempted to import and can't find your course content, double check your destination and origin courses before trying to import again. This helps avoid accidentally importing a course twice.<br>
+Visit <a href="https://portal.cca.edu/knowledge-base/moodle/importing-a-course-or-course-content-into-moodle/">Importing a Course or Course Content into Moodle</a> on Portal for a detailed guide.</div>`
+    let div = document.createElement('div')
+    div.innerHTML = msg
+    let target = document.getElementById('maincontent')
+    if (target) target.insertAdjacentElement('beforebegin', div)
 }
