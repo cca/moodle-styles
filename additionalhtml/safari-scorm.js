8c192673a619041fa36f377f3ef162a0d170ad28
phette23 <phette23@gmail.com>
2023-08-17 09:05:45 -0700

    prevent Safari browser from doing SCORM activities
    ref #36

---
+++ additionalhtml/safari-scorm.js
@@ -0,0 +1,19 @@
+// #36 SCORM progress doesn't work in Safari
+// two questions: 1) should we _prevent_ users from even starting the SCORM?
+// 2) should we target the "Personal Skills for a Diverse Campus" SCORM specifically, by checking against the page title, or all SCORMs?
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+if (location.pathname.match('/mod/scorm/view.php') && isSafari) {
+    // let some of the page load before interrupting it
+    // using window.addEventListener('load',...) can cause an AJAX error in Moodle
+    document.addEventListener('DOMContentLoaded', () => {
+        alert('This activity does not work on Safari. Please use another browser, such as Mozilla Firefox or Google Chrome.')
+        // monitor for button and disable it
+        const interval = setInterval(() => {
+            let btn = document.getElementById('n')
+            if (btn) {
+                btn.disabled = true
+                clearInterval(interval)
+            }
+        }, 500)
+    })
+}
