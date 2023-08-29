409f60a69a77f0822cdb52bc1eb4fdbb44de9fd1
phette23 <phette23@gmail.com>
2023-08-29 12:07:43 -0700

    don't show name change warning to 'ghost' users
    and I found a way to fix the ghost user problem so we don't need that
    error alert anymore, we let users fill out their names and email if
    those fields are empty

--- additionalhtml/name-change-warning.js
+++ additionalhtml/name-change-warning.js
@@ -1,12 +1,17 @@
 // run when anyone edits a profile
 let page = location.pathname
 if (page.match('/user/edit.php') || page.match('/user/editadvanced.php')) {
-    let showNameChangeWarning = () => {
-        const html = '<div class="col-md-9 alert alert-primary" style="margin:6px;"><button type="button" class="close" data-dismiss="alert">×</button> We recommend notifying your instructors or students after changing your name, to avoid any confusion.</div>'
-        // we should have jQuery by the time this happens
-        $(event.target.parentElement).after(html)
+    // if it's a "ghost" user with no names or email, don't bother warning them
+    const d = document
+    const notGhost = d.getElementById('id_firstname').value || d.getElementById('id_lastname').value
+    if (notGhost) {
+        const showNameChangeWarning = (ev) => {
+            const html = '<div class="col-md-9 alert alert-primary" style="margin:6px;"><button type="button" class="close" data-dismiss="alert">×</button> We recommend notifying your instructors or students after changing your name, to avoid any confusion.</div>'
+            // we should have jQuery by the time this happens
+            $(ev.target.parentElement).after(html)
+        }
+        d.querySelectorAll('#id_firstname, #id_lastname').forEach(el => {
+            el.addEventListener('change', showNameChangeWarning, { once: true })
+        })
     }
-    document.querySelectorAll('#id_firstname, #id_lastname').forEach(el => {
-        el.addEventListener('change', showNameChangeWarning, { once: true })
-    })
 }
