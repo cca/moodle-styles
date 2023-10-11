e8cb7cd84051f46179f1c80b0229c14aef3ab770
phette23 <phette23@gmail.com>
2023-10-10 17:07:47 -0700

    use kubectl and admin/cli/cfg.php to directly apply customizations
    closes #22, there was no way to use the Moodle APIs to do this but it works
    smoothly by using kubectl to remotely execute the cfg.php utility

---
+++ scripts/cfgset.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+SETTING="$1"
+FILE="$2"
+
+# requires a NS k8s namespace var
+if [[ $NS != 'moo-stg1' && $NS != 'moo-prod' ]]; then
+    echo "ERROR: requires an NS namespace env var matching either the production or staging namespace"
+    exit 1
+fi
+if [[ -n $SETTING && -f $FILE ]]; then
+    POD=$(kubectl --namespace $NS get pods -l app.kubernetes.io/name=moodle -o name)
+    echo "Setting $SETTING on $POD to the contents of file $FILE"
+    kubectl --namespace $NS exec $POD -- php /bitnami/moodle/admin/cli/cfg.php --name="$SETTING" --set="$(cat $FILE)"
+else
+    echo "ERROR: first argument must be a Moodle core setting string and second must be a file whose contents are the value of that setting."
+fi
---
+++ scripts/scss.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+FILE="$1"
+
+# requires a NS k8s namespace var
+if [[ $NS != 'moo-stg1' && $NS != 'moo-prod' ]]; then
+    echo "ERROR: requires an NS namespace env var matching either the production or staging namespace"
+    exit 1
+fi
+if [[ -f $FILE ]]; then
+    POD=$(kubectl --namespace $NS get pods -l app.kubernetes.io/name=moodle -o name)
+    echo "Setting theme_boost scss on $POD to the contents of file $FILE"
+    kubectl --namespace $NS exec $POD -- php /bitnami/moodle/admin/cli/cfg.php --component="theme_boost" --name="scss" --set="$(cat $FILE)"
+else
+    echo "ERROR: argument must be a SCSS file"
+fi
