4bd05632950a2385455e42110eb95c91ffbe8d5e
phette23 <phette23@gmail.com>
2024-02-27 16:15:55 -0800

    fix: repeated ISS review Saves don't trigger multiple emails
    closes #45

--- additionalhtmlhead/iss-lock-btn.js
+++ additionalhtmlhead/iss-lock-btn.js
@@ -6,7 +6,6 @@ if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 
     // notify user of error, fall back to browser alert if we can't load Moodle's notification library
     // see additionalhtmlhead/readme.md for how Moodle notifications work
     const errorAlert = () => {
-        // TODO longer than average timeout
         if (require) {
             return require([ 'core/notification' ], function (notification) {
             notification.alert("Error", "Locking the assignment failed, no notification was sent to <abbr title='International Student Services'>ISS</abbr>. Please manually lock the submission (View all submissions > Edit > Prevent submission changes) and let <a href='https://portal.cca.edu/help-desk/' target='_blank'>the Help Desk</a> know about this error.")
@@ -18,28 +17,30 @@ if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 
     // lock assignment if grade is ISS Review & notify user
     const handler = () => {
         // only lock if grade is ISS Review
-        if (document.querySelector('#id_grade').value.trim().toLowerCase() !== '2') {
+        let grade = document.querySelector('#id_grade')
+        if (grade && grade.value.trim() != '2') {
             return console.log('Not locking because grade is not ISS Review')
         }
 
         // the query string can change without the page reloading if you arrow to another student
         params = new URLSearchParams(location.search)
         // lock URLs look like
-        // https://moodle.cca.edu/mod/assign/view.php?id=236146&userid=4453&action=lock&sesskey=vKW7E2y8Kn&page=0
-        // where ID appears to be the Assignment ID and user ID is the student's ID
-        fetch(`https://${location.hostname}/mod/assign/view.php?id=${params.get('id')}&userid=${params.get('userid')}&action=lock&sesskey=${M.cfg.sesskey}`)
+        // /mod/assign/view.php?id=236146&userid=4453&action=lock&sesskey=vKW7E2y8Kn&page=0
+        // where `id` is the assignment's course module ID and `userid` is the student's ID
+        let assignment = params.get('id')
+        let student = params.get('userid')
+        let msg = `Locking assignment https://${location.hostname}/mod/assign/view.php?id=${assignment} for student https://${location.hostname}/user/profile.php?id=${student}`
+        fetch(`https://${location.hostname}/mod/assign/view.php?id=${assignment}&userid=${student}&action=lock&sesskey=${M.cfg.sesskey}`)
             .then(response => {
                 if (response.ok) {
-                    // TODO longer than average timeout
                     require([ 'core/toast' ], function (toast) {
-                        toast.add("ISS has been notified.", { type: 'success' })
+                        toast.add("ISS has been notified.", { delay: 6000, type: 'success' })
                     })
-                    // TODO log IDs to console
-                    console.log('Locking successful')
+                    console.log(`${msg} was successful.`)
                     // do not submit the form, the form that wraps the buttons has no inputs or action URL
                     // it leads to an error page if you submit it
                 } else {
-                    console.error('Locking failed', response)
+                    console.error(`${msg}  failed`, response)
                     errorAlert()
                 }
             })
@@ -50,18 +51,18 @@ if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 
     }
 
     const monitorAndAddHandler = () => {
-        let target = document.querySelector('button[name="savechanges"]')
         let locked = document.querySelector('.submissionnoteditable')
-        // TODO if it's locked, should we remove the handler?
-        // target exists, there's no button, & assignment isn't already locked
-        if (target && !locked) {
-            // reset the handler (could have navigated to another student)
-            target.removeEventListener('click', handler)
-            target.addEventListener('click', handler)
-            // do the same for the "Save and show next" button
-            let saveandshownext = document.querySelector('button[name="saveandshownext"]')
-            saveandshownext.removeEventListener('click', handler)
-            saveandshownext.addEventListener('click', handler)
+        let save = document.querySelector('button[name="savechanges"]')
+        let saveandshownext = document.querySelector('button[name="saveandshownext"]')
+        if (save) {
+            // either way, we remove any existing handlers
+            save.removeEventListener('click', handler)
+            if (saveandshownext) saveandshownext.removeEventListener('click', handler)
+            if (!locked) {
+                // if the assignment isn't already locked, we add them again
+                save.addEventListener('click', handler)
+                if (saveandshownext) saveandshownext.addEventListener('click', handler)
+            }
         }
     }
 
