0a9da3dc989f99daf7f5db32c9bbf1f965536fd7
phette23 <phette23@gmail.com>
2024-04-10 16:30:23 -0700

    fix: make scales-helper work with Forum grades

--- additionalhtml/scales-helper.js
+++ additionalhtml/scales-helper.js
@@ -6,7 +6,7 @@ if (location.pathname.match('/course/modedit.php')) {
     let warning = `<div id="${id}" class="alert alert-warning hidden">Attention: Please read <a href="https://portal.cca.edu/teaching/teaching-lab/teaching-resources/teaching-and-learning-technologies/moodle/grading-tracking/using-scales-when-grading-faculty/#how-does-moodle-calculate-a-scale" target="_blank">How Moodle Calculates Scales</a> before using a scale.</div>`
 
     d.addEventListener('DOMContentLoaded', () => {
-        let gtElement = d.querySelector('#id_grade_modgrade_type')
+        let gtElement = d.getElementById('id_grade_modgrade_type') || d.getElementById('id_grade_forum_modgrade_type')
         // if we want to warn against only certain specific scales we need to
         // listen to the #id_grade_modgrade_scale select for its blur event
         // and then check the textContent of its selected <option>
@@ -16,7 +16,7 @@ if (location.pathname.match('/course/modedit.php')) {
                 let gradeType = $(event.target).val().trim()
 
                 // add hidden warning to DOM on first blur
-                if (!d.getElementById(id)) $('#fitem_fgroup_id_grade .form-inline').append(warning)
+                if (!d.getElementById(id)) $('#fitem_fgroup_id_grade .form-inline, #fitem_fgroup_id_grade_forum .form-inline').append(warning)
 
                 if (gradeType === 'scale') {
                     return $(`#${id}`).show()
