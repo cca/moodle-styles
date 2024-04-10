7f7f018f1050900b18c5abd1aebc07e4b452ae97
phette23 <phette23@gmail.com>
2024-04-10 16:11:03 -0700

    fix: remove UA analytics code
    UA properties haven't worked for a while
    this also should stop tracking on staging which we don't want

--- additionalhtml/ga.js
+++ additionalhtml/ga.js
@@ -1,12 +1,5 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-131604-9', 'auto');
-ga('send', 'pageview');
-
-// GA4 - use both snippets for now to keep our UA & GA4 properties simultaneously
-window.dataLayer = window.dataLayer || [];
+// Staging lacks the GTM script tag so gtag() calls just sit in the dataLayer array
+window.dataLayer = window.dataLayer || []
 window.gtag = function(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-9KK2VCY0TM', { 'anonymize_ip': true, 'transport_type': 'beacon' });
