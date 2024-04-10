30b5f5e7a37fafdb860aa7532b0b39bd3a89d989
phette23 <phette23@gmail.com>
2024-04-10 16:03:54 -0700

    fix: remove feedback-preview, bug fixed upstream
    Ref: https://tracker.moodle.org/browse/MDL-70586

--- additionalhtml/feedback-preview.js
+++
@@ -1,6 +0,0 @@
-// hide Feedback activity's "Preview questions" button from students
-// https://tracker.moodle.org/browse/MDL-70586
-// only show if "Settings" menu link is not present (as for students)
-if (location.pathname.match('/mod/feedback/view.php') && !document.querySelector('.secondary-navigation .moremenu li[data-key="modedit"]')) {
-    document.querySelector('.feedback_description .tertiary-navigation a[href*="/mod/feedback/print.php?id="]').remove()
-}
