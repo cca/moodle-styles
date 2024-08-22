47e979a28b69160f1573fdc59e4a871b2ddde0e5
phette23 <phette23@gmail.com>
2024-08-22 10:57:42 -0700

    fix: work on /uploaduser/ path w/o index.php

--- additionalhtml/uploaduser.js
+++ additionalhtml/uploaduser.js
@@ -1,5 +1,5 @@
 // https://moodle.cca.edu/admin/tool/uploaduser/index.php
-if (location.pathname.match('/admin/tool/uploaduser/index.php')) {
+if (location.pathname.match('/admin/tool/uploaduser/')) {
     // remove any "Default values" (cannot figure out how to do with via admin settings!)
     document.querySelectorAll('#id_defaultheader input[type="text"]').forEach((el) => {
         el.value = ''
