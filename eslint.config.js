2be599df7707a9a78fad2f68603e6a96c8d576c1
phette23 <phette23@gmail.com>
2024-10-22 10:05:52 -0700

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import globals from 'globals'
+import js from "@eslint/js"
+
+export default [
+    { ignores: ["build/**"], },
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            globals: {
+                ...globals.browser,
+                // google analytics globals
+                ga: 'writable',
+                gtag: 'readonly',
+                dataLayer: 'writable',
+                // Moodle & jQuery
+                M: 'readonly',
+                $: 'readonly',
+            },
+            sourceType: 'script',
+        },
+        rules: {
+            indent: [ 'warn', 4 ],
+            'linebreak-style': [ 'error', 'unix' ],
+            'no-unused-vars': 'off',
+            'no-empty': 'off',
+            'quotes': [ 'warn', 'single' ],
+            'semi': [ 'error', 'never' ],
+        }
+    },
+    // gulpfile is commonjs
+    {
+        files: ['gulpfile.js', ],
+        languageOptions: {
+            globals: { ...globals.node, },
+            sourceType: 'commonjs',
+        }
+    },
+    // this file
+    {
+        files: ['eslint.config.js', ],
+        languageOptions: {
+            globals: { ...globals.node, },
+            sourceType: 'module',
+        },
+    }
+]
