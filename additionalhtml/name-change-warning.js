7b4143bf084aeea34a1b04151b3176eaf6cb37d8
phette23 <phette23@gmail.com>
2024-11-12 11:13:17 -0800

    fix: name warning needs to be vanilla js
    jquery not available when this runs

--- additionalhtml/name-change-warning.js
+++ additionalhtml/name-change-warning.js
@@ -1,7 +1,8 @@
 // run when anyone edits a profile
 let page = location.pathname
 if (page.match('/user/edit.php') || page.match('/user/editadvanced.php')) {
-    const html = '<div class="col-md-9 alert alert-primary" style="margin:6px;"><button type="button" class="close" data-dismiss="alert">×</button> Your name and email come from Workday and cannot be edited here. <a href="https://portal.cca.edu/essentials/workday-student/my-profile-information/updating-name-and-personal-information-step-step-guide/#-edit-your-preferred-name-">See Portal for information on changing your preferred name</a>.</div>'
-    // we should have jQuery by the time this happens
-    $('#id_moodlecontainer').after(html)
+    const d = document
+    let div = d.createElement('div')
+    div.innerHTML = '<p class="col-md-9 alert alert-primary" style="margin:6px;"><button type="button" class="close" data-dismiss="alert">×</button> Your name and email come from Workday and cannot be edited here. <a href="https://portal.cca.edu/essentials/workday-student/my-profile-information/updating-name-and-personal-information-step-step-guide/#-edit-your-preferred-name-">See Portal for information on changing your preferred name</a>.</p>'
+    d.getElementById('id_moodlecontainer').prepend(div)
 }
