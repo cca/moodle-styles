49bccf6382f5574c6586bfb3100f862db6c65056
phette23 <phette23@gmail.com>
2025-04-30 14:05:27 -0700

    fix: annotated pdf viewer link changed
    .visibleifjs selector needed to be removed

--- additionalhtml/anno-bib-viewer.js
+++ additionalhtml/anno-bib-viewer.js
@@ -3,7 +3,7 @@
 // We simply remove the link (students can download the PDF)
 const params = new URLSearchParams(location.search)
 if (location.pathname.match('/mod/assign/view.php') && params.get('action') === 'view') {
-    document.querySelectorAll('[class*="summary_assignfeedback_editpdf"] .visibleifjs a').forEach(a => {
+    document.querySelectorAll('[class*="summary_assignfeedback_editpdf"] a').forEach(a => {
         if (a.textContent.toLowerCase().match('view annotated pdf')) a.remove()
     })
 }
