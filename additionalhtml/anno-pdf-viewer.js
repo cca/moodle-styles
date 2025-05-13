e0afe37413e68d6beb0eda89b905a9d9e975052d
phette23 <phette23@gmail.com>
2025-05-13 16:31:35 -0700

    docs: note about pdf viewer bug fix

--- additionalhtml/anno-pdf-viewer.js
+++ additionalhtml/anno-pdf-viewer.js
@@ -1,5 +1,6 @@
 // Work around https://tracker.moodle.org/browse/MDL-78886
 // Page back/forward links in annotated bib viewer don't work
+// Bug is fixed but not in a release (as of 4.4.8) yet
 // We simply remove the link (students can download the PDF)
 if (location.pathname.match('/mod/assign/view.php')) {
     document.querySelectorAll('[class*="summary_assignfeedback_editpdf"] a').forEach(a => {
