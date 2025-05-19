0aaced9beaadfe5738964597cb9a8263c19847f0
phette23 <phette23@gmail.com>
2025-05-19 12:43:01 -0700

    style: rm semicolon

--- additionalhtml/env-switcher.js
+++ additionalhtml/env-switcher.js
@@ -6,5 +6,5 @@ if (window.location.hostname === stagingDomain) {
     const menuHtml = `<li class="nav-item" role="none" data-forceintomoremenu="false">
     <a role="menuitem" class="nav-link" href="${url}" data-disableactive="true">Production</a>
 </li>`
-    document.querySelector('.primary-navigation nav ul').insertAdjacentHTML('beforeend', menuHtml);
+    document.querySelector('.primary-navigation nav ul').insertAdjacentHTML('beforeend', menuHtml)
 }
