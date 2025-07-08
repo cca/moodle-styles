a7b6b91e1a3bd732d0691c957160605c61f31a7c
phette23 <phette23@gmail.com>
2025-07-08 08:21:17 -0700

    fix: rm anno-pdf-viewer.js, fixed in 4.4.9

--- additionalhtml/anno-pdf-viewer.js
+++
@@ -1,9 +0,0 @@
-// Work around https://tracker.moodle.org/browse/MDL-78886
-// Page back/forward links in annotated bib viewer don't work
-// Bug is fixed but not in a release (as of 4.4.8) yet
-// We simply remove the link (students can download the PDF)
-if (location.pathname.match('/mod/assign/view.php')) {
-    document.querySelectorAll('[class*="summary_assignfeedback_editpdf"] a').forEach(a => {
-        if (a.textContent.toLowerCase().match('view annotated pdf')) a.remove()
-    })
-}
